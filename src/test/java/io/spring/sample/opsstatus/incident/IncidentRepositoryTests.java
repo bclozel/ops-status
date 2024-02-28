6883efbd3c558cd35df62d27822990aa6fe7ac4f
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-02-28 09:50:33 +0100

    Basic Testcontainers demo
    This commit uses Testcontainers for the test so that a database should
    not be available for it to succeed. We use `@DynamicPropertySource` to
    inject the credentials exposed by the container so that Spring Boot is
    configured to use it.
    
    This also removes any reference of H2 and our duplicate management of
    the schema.

--- src/test/java/io/spring/sample/opsstatus/incident/IncidentRepositoryTests.java
+++ src/test/java/io/spring/sample/opsstatus/incident/IncidentRepositoryTests.java
@@ -5,16 +5,35 @@ import java.util.List;
 
 import io.spring.sample.opsstatus.faker.DemoDataGenerator;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJdbcTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Testcontainers(disabledWithoutDocker = true)
 class IncidentRepositoryTests {
+
+	@Container
+	static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.6-alpine");
+
+	@DynamicPropertySource
+	static void registerPgProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	private final IncidentRepository repository;
 
