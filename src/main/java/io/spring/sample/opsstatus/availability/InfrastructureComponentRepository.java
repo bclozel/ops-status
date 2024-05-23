2c5427a10b36bc1f5845d07c1c04e981dfe17d61
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-05-23 19:44:16 +0200

    Migrate to Spring Boot 3.0.x
    Our app is affected by Spring Data's restructuring of their repository
    interfaces, and we need to explicitly extends from an additional
    interface to get CRUD support.

--- src/main/java/io/spring/sample/opsstatus/availability/InfrastructureComponentRepository.java
+++ src/main/java/io/spring/sample/opsstatus/availability/InfrastructureComponentRepository.java
@@ -1,7 +1,9 @@
 package io.spring.sample.opsstatus.availability;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface InfrastructureComponentRepository extends PagingAndSortingRepository<InfrastructureComponent, Long> {
+public interface InfrastructureComponentRepository extends CrudRepository<InfrastructureComponent, Long>,
+		PagingAndSortingRepository<InfrastructureComponent, Long> {
 
 }
