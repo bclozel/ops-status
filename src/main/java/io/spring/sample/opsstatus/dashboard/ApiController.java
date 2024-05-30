1678a019f46ebc6af5f6afed8625c894065a1006
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-05-30 10:35:57 +0200

    Set up per-media type error handling
    As of Spring Framework 6.2.0, the `@ExceptionHandler` annotation
    supports a new `produces` attribute that allows to refine which
    exception handler will be used depending on the media types accepted by
    the HTTP client.
    
    Here, browsers will get an HTML view and client apps will get an
    "application/problem+json" error response.

--- src/main/java/io/spring/sample/opsstatus/dashboard/ApiController.java
+++ src/main/java/io/spring/sample/opsstatus/dashboard/ApiController.java
@@ -3,6 +3,7 @@ package io.spring.sample.opsstatus.dashboard;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 import io.spring.sample.opsstatus.incident.Incident;
 import io.spring.sample.opsstatus.incident.IncidentOrigin;
@@ -11,10 +12,14 @@ import io.spring.sample.opsstatus.incident.IncidentStatus;
 import io.spring.sample.opsstatus.incident.ItemType;
 import io.spring.sample.opsstatus.incident.ReportItem;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/api")
@@ -36,6 +41,12 @@ public class ApiController {
 		return this.repository.findById(id)
 			.map(IncidentInfo::fromIncident)
 			.orElseThrow(() -> new MissingIncidentException(id));
+	}
+
+	@ExceptionHandler(produces = "text/html")
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ModelAndView handleHtmlError(MissingIncidentException ex) {
+		return new ModelAndView("apierror", Map.of("exception", ex));
 	}
 
 	public record IncidentsDescriptor(List<IncidentInfo> incidents) {
--- src/test/java/io/spring/sample/opsstatus/dashboard/ApiControllerTests.java
+++ src/test/java/io/spring/sample/opsstatus/dashboard/ApiControllerTests.java
@@ -31,7 +31,9 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(controllers = ApiController.class)
 class ApiControllerTests {
@@ -81,6 +83,16 @@ class ApiControllerTests {
 				.andExpect(status().is(HttpStatus.NOT_FOUND.value()))
 				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_PROBLEM_JSON))
 				.andExpect(content().json(readJson(new ClassPathResource("missing-incident-42.json", getClass())),true));
+	}
+
+	@Test
+	void incidentWithNoSuchIncidentErrorView() throws Exception {
+		when(this.incidentRepository.findById(42L)).thenReturn(Optional.empty());
+		mvc.perform(get("/api/incidents/42").accept(MediaType.TEXT_HTML))
+				.andExpect(status().is(HttpStatus.NOT_FOUND.value()))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+				.andExpect(view().name("apierror"))
+				.andExpect(model().attributeExists("exception"));
 	}
 
 	private Matcher<?> jsonIncidentMatcher(Incident incident) {
